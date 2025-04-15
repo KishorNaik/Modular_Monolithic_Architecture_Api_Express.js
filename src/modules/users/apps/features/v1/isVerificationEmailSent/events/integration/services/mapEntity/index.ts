bd0becca40db454f170b22715c820e99fe7c3b7e
Kishor Naik <kishor.naik011.net@gmail.com>
2025-04-15 16:16:00 +0530

    isVerificationEmailSendUpdateDbServiceResult

--- src/modules/users/apps/features/v1/isVerificationEmailSent/events/integration/services/mapEntity/index.ts
+++ src/modules/users/apps/features/v1/isVerificationEmailSent/events/integration/services/mapEntity/index.ts
@@ -1,36 +1,36 @@
-import { IUsers } from "@/modules/users/shared/types";
-import { sealed } from "@/shared/utils/decorators/sealed";
-import { ResultError, ResultExceptionFactory } from "@/shared/utils/exceptions/results";
-import { IServiceHandlerAsync } from "@/shared/utils/helpers/services";
-import { UserSettingsEntity } from "@kishornaik/mma_db";
-import { BoolEnum } from "@kishornaik/mma_db/dist/core/shared/models/enums/bool.enum";
-import { StatusCodes } from "http-status-codes";
-import { Ok, Result } from "neverthrow";
-import { Service } from "typedi";
+import { IUsers } from '@/modules/users/shared/types';
+import { sealed } from '@/shared/utils/decorators/sealed';
+import { ResultError, ResultExceptionFactory } from '@/shared/utils/exceptions/results';
+import { IServiceHandlerAsync } from '@/shared/utils/helpers/services';
+import { UserSettingsEntity } from '@kishornaik/mma_db';
+import { BoolEnum } from '@kishornaik/mma_db/dist/core/shared/models/enums/bool.enum';
+import { StatusCodes } from 'http-status-codes';
+import { Ok, Result } from 'neverthrow';
+import { Service } from 'typedi';
 
-export interface IIsVerificationEmailSendIntegrationEventMapEntityService extends IServiceHandlerAsync<IUsers,UserSettingsEntity>{}
+export interface IIsVerificationEmailSendIntegrationEventMapEntityService
+	extends IServiceHandlerAsync<IUsers, UserSettingsEntity> {}
 
 @sealed
 @Service()
-export class IsVerificationEmailSendIntegrationEventMapEntityService implements IIsVerificationEmailSendIntegrationEventMapEntityService{
-  public async handleAsync(params: IUsers): Promise<Result<UserSettingsEntity, ResultError>> {
-    try
-    {
-      //@guard
-      if(!params)
-        return ResultExceptionFactory.error(StatusCodes.BAD_REQUEST,'Invalid params');
+export class IsVerificationEmailSendIntegrationEventMapEntityService
+	implements IIsVerificationEmailSendIntegrationEventMapEntityService
+{
+	public async handleAsync(params: IUsers): Promise<Result<UserSettingsEntity, ResultError>> {
+		try {
+			//@guard
+			if (!params)
+				return ResultExceptionFactory.error(StatusCodes.BAD_REQUEST, 'Invalid params');
 
-      // Map Entity
-      const userSettingsEntity=new UserSettingsEntity();
-      userSettingsEntity.identifier=params.settings.identifier;
-      userSettingsEntity.isVerificationEmailSent=BoolEnum.YES;
+			// Map Entity
+			const userSettingsEntity = new UserSettingsEntity();
+			userSettingsEntity.identifier = params.settings.identifier;
+			userSettingsEntity.isVerificationEmailSent = BoolEnum.YES;
 
-      return new Ok(userSettingsEntity);
-    }
-    catch(ex){
-      const error=ex as ResultError;
-      return ResultExceptionFactory.error(StatusCodes.INTERNAL_SERVER_ERROR,error.message);
-    }
-  }
-
+			return new Ok(userSettingsEntity);
+		} catch (ex) {
+			const error = ex as ResultError;
+			return ResultExceptionFactory.error(StatusCodes.INTERNAL_SERVER_ERROR, error.message);
+		}
+	}
 }
--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -2,7 +2,10 @@ import Container from 'typedi';
 import { CreateUserController } from './apps/features/v1/createUsers';
 import { UpdateUserSettingsService, UpdateUserVersionService } from '@kishornaik/mma_db';
 
-Container.set<UpdateUserSettingsService>(UpdateUserSettingsService,new UpdateUserSettingsService());
-Container.set<UpdateUserVersionService>(UpdateUserVersionService,new UpdateUserVersionService());
+Container.set<UpdateUserSettingsService>(
+	UpdateUserSettingsService,
+	new UpdateUserSettingsService()
+);
+Container.set<UpdateUserVersionService>(UpdateUserVersionService, new UpdateUserVersionService());
 
 export const userModule: Function[] = [CreateUserController];
