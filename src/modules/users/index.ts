41b15859efa3a5fd2f56255ce50b8ac00eb88a02
Kishor Naik <kishor.naik011.net@gmail.com>
2025-04-15 16:15:31 +0530

    @KishorNaik: Update is Verified Email and Update Cache

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -1,3 +1,8 @@
+import Container from 'typedi';
 import { CreateUserController } from './apps/features/v1/createUsers';
+import { UpdateUserSettingsService, UpdateUserVersionService } from '@kishornaik/mma_db';
+
+Container.set<UpdateUserSettingsService>(UpdateUserSettingsService,new UpdateUserSettingsService());
+Container.set<UpdateUserVersionService>(UpdateUserVersionService,new UpdateUserVersionService());
 
 export const userModule: Function[] = [CreateUserController];
