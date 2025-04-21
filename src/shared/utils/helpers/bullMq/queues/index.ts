78874d2d03a5a2c61630b7b709b4ec6b911251e5
Kishor Naik <kishor.naik011.net@gmail.com>
2025-04-21 15:36:20 +0530

    @KishorNaik: Added attemp

--- src/shared/utils/helpers/bullMq/queues/index.ts
+++ src/shared/utils/helpers/bullMq/queues/index.ts
@@ -18,7 +18,7 @@ export const publishQueuesAsync = <T extends Object>(
 	return queue.add(jobName, data as T, {
 		removeOnComplete: true,
 		removeOnFail: true,
-    attempts:3
+		attempts: 3,
 	});
 };
 
